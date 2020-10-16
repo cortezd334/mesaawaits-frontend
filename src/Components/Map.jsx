968d5aa52847e4957f020f169267bc68ab9f6edd
cortezd334 <cortezd334@gmail.com>
2020-10-15 20:42:05 -0700

    markers

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,89 +1,70 @@
-import React from 'react';
-import { GoogleMap, LoadScript, Marker, useLoadScript } from '@react-google-maps/api';
+import React, { useState, useEffect } from 'react';
+import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api';
 
-// const containerStyle = {
-//     width: '600px',
-//     height: '600px'
-//   };
-   
-//   const center = {
-//     lat: 37.7749,
-//     lng: 122.4194
-//   };
-
-function Map({center}) {
+function Map({restaurants}) {
   
-    const containerStyle = {
+  const containerStyle = {
     height: "400px",
     width: "800px"
-    }
+  }
   
-    // const [center, setCenter] = useState({
-    // lat: 37.7599,
-    // lng: -122.4148
-    // })
+  const [center, setCenter] = useState({
+  lat: 37.7599,
+  lng: -122.4148
+  })
 
+  useEffect(() => {
+    getLocation()
+  }, [restaurants]);
+
+  const [markers, setMarkers] = useState([])
+
+  function restMarkers() {
+    return restaurants.map(restaurant => {
+        const rest = restaurant.coordinates
+        return <Marker position={{lat: rest.latitude, lng: rest.longitude}}></Marker>
+        // return <Marker onLoad={marker => console.log(marker)} position={{lat: rest.latitude, lng: rest.longitude}}></Marker>
+    });
+  }
+
+  const onMapClick = React.useCallback((e) => {
+    // displayRest()
+    setMarkers(() => [{
+      lat: e.latLng.lat(),
+      lng: e.latLng.lng(),
+    }])
+  }, [])
+
+  
   const {isLoaded, loadError} = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY
   })
-
+  
   const mapRef = React.useRef()
   const onMapLoad = React.useCallback((map) => {
     mapRef.current = map
-    // getLocation()
   }, [])
-
+  
   if(loadError) return 'Error'
   if(!isLoaded) return 'Maps'
-
-  // function getLocation() {
-  //   if (navigator.geolocation) {
-  // navigator.geolocation.getCurrentPosition(showPosition);
-  // }
-
-  // function showPosition(position) {
-  //   setCenter({lat: position.coords.latitude, lng: position.coords.longitude})
-  // }
-  
-    // const [map, setMap] = useState(null)
-    // const [center, setCenter] = useState({
-    //   lat: 37.7749,
-    //   lng: 122.4194
-    // })
-
-    // const API_KEY = process.env.REACT_APP_GOOGLE_KEY
-
-    // const onLoad = React.useCallback(function callback(map) {
-    //   const bounds = new window.google.maps.LatLngBounds();
-    //   map.fitBounds(bounds);
-    //   setMap(map)
-    // }, [])
-   
-    // const onUnmount = React.useCallback(function callback(map) {
-    //   setMap(null)
-    // }, [])
-
-    // function getLocation() {
-    //     if (navigator.geolocation) {
-    //       navigator.geolocation.getCurrentPosition(showPosition);
     
-    //     } else {
-    //       x.innerHTML = "Geolocation is not supported by this browser.";
-    //     }
-    //   }
-    
-    
-    // let x = document.getElementById("demo");
-    // function showPosition(position) {
+  function restMapLocation() {
+    let first = restaurants[0].coordinates
+    const location = {lat: first.latitude, lng: first.longitude}
+    setCenter(location)
+  }
 
-    // setCenter({lat: position.coords.latitude, lng: position.coords.longitude})
+  function showPosition(position) {
+    setCenter({lat: position.coords.latitude, lng: position.coords.longitude})
+  }
 
-    // let latlon = position.coords.latitude + "," + position.coords.longitude;
-    
-    // let img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&sensor=false&key=API_KEY";
-    
-    // // document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
-    // }
+  function getLocation() {
+    if(restaurants.length > 0) {
+      restMapLocation()
+    } else if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition)
+    }
+  }
 
     return (
       <GoogleMap
@@ -91,60 +72,9 @@ function Map({center}) {
         center={center}
         zoom={12}
         onLoad={onMapLoad}
-      >
+        >
+        {restMarkers()}
       </GoogleMap>
-        // <LoadScript
-        // googleMapsApiKey={API_KEY}
-        // >
-        // { getLocation() }
-        //   <GoogleMap
-        //     mapContainerStyle={containerStyle}
-        //     center={center}
-        //     zoom={2}
-        //     onLoad={onLoad}
-        //     onUnmount={onUnmount}
-        //   >
-        //     { /* Child components, such as markers, info windows, etc. */ }
-        //     <></>
-        //   </GoogleMap>
-        // </LoadScript>
     );
 }
-// export default React.memo(Map)
-export default Map
-
-// const { GoogleMap, LoadScript } = require("../../");
-// const ScriptLoaded = require("../../docs/ScriptLoaded").default;
-
-// const mapContainerStyle = {
-//   height: "400px",
-//   width: "800px"
-// }
-
-// const center = {
-//   lat: 0,
-//   lng: -180
-// }
-
-// const position = {
-//   lat: 37.772,
-//   lng: -122.214
-// }
-
-// const onLoad = marker => {
-//   console.log('marker: ', marker)
-// }
-
-// <ScriptLoaded>
-//   <GoogleMap
-//     id="marker-example"
-//     mapContainerStyle={mapContainerStyle}
-//     zoom={2}
-//     center={center}
-//   >
-//     <Marker
-//       onLoad={onLoad}
-//       position={position}
-//     />
-//   </GoogleMap>
-// </ScriptLoaded>
+export default React.memo(Map)
