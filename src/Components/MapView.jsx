7d85765fcd98e66dd1ac3f6c0535db0fa0408ead
cortezd334 <cortezd334@gmail.com>
2020-10-21 21:35:41 -0700

    rest cards

--- src/Components/MapView.jsx
+++ src/Components/MapView.jsx
@@ -6,7 +6,7 @@ import { saveRestaurant, addFavorite, delFavorite } from '../api';
 import utensils_icon from '../images/utensils_icon.png'
 import new_heart from '../images/new_heart.png'
 import red_heart from '../images/red_heart.png'
-import Card from 'react-bootstrap/Card';
+import Media from 'react-bootstrap/Media';
 import Button from 'react-bootstrap/Button';
 
 // import Restaurant from './Restaurant';
@@ -20,26 +20,24 @@ export default function MapView({restaurants, center, getLocation, user, setUser
             let cuisine = restaurant.categories.map(cuisine => {
                 return cuisine.title
             })
-            return <Card key={restaurant.id}>
-                <div className='imgCon'>
-                    <img className='img' src={restaurant.image_url} alt='Restaurant'/>
-                </div>
-                <div>
-                    <h3>{restaurant.name}</h3>
-                    <br/>
-                    <p>{restaurant.rating} Star Rating</p>
-                    <p>{` ${cuisine.join(', ')} `}</p>
-                </div>
-                <div>
-                <Button variant="primary" onClick={() => clickHandler(restaurant)}>Make Reservation</Button>
-                </div>
-                <div>
-                    {user.favorites.map(favorite => 
-                        favorite.restaurant.name).includes(restaurant.name) ? 
-                    <img className='icon' src={red_heart} alt='Fav Heart' onClick={() => handleDelete(restaurant)}/> :
-                    <img className='icon' src={new_heart} alt='New Heart' onClick={() => handleAdd(restaurant)}/>}
-                </div>
-            </Card>
+            return <>
+                <Media className='imgCon' key={restaurant.id}>
+                    <img className='mr-3 img' src={restaurant.image_url} alt='restaurant'/>
+                    <Media.Body className='info'>
+                        <h3>{restaurant.name}</h3>
+                        <br/>
+                        <p>{restaurant.rating} Star Rating</p>
+                        <p>{` ${cuisine.join(', ')} `}</p>
+                        <div>
+                            <Button variant="primary" onClick={() => clickHandler(restaurant)}>Make Reservation</Button>
+                            {user.favorites.map(favorite => 
+                                favorite.restaurant.name).includes(restaurant.name) ? 
+                            <img className='icon' src={red_heart} alt='Fav Heart' onClick={() => handleDelete(restaurant)}/> :
+                            <img className='icon' src={new_heart} alt='New Heart' onClick={() => handleAdd(restaurant)}/>}
+                        </div>
+                    </Media.Body>
+                </Media>
+            </>
         })
     }
 
