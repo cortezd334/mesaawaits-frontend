7275cf2f9f96b83eaf3885c39d68ad596947ba3e
cortezd334 <cortezd334@gmail.com>
2021-02-16 20:11:01 -0800

    unused components

--- src/Components/searchContext.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const SearchContext = React.createContext()
-
-const SearchProvider = SearchContext.Provider
-const SearchConsumer = SearchContext.Consumer
-
-export { SearchProvider, SearchConsumer }
